c0eeaadf995af8a3535971d9a0b87a2030969cba
pius-bro <osiemoconcepta@gmail.com>
2025-03-03 10:56:58 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("colorButton").addEventListener("click", function() {
+    const colors = ["#FF5733", "#33FF57", "#3357FF", "#F033FF", "#FF8C33"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+  });
+  document.getElementById("colorButton").addEventListener("click", function() {
+    const colors = ["#FF5733", "#33FF57", "#3357FF", "#F033FF", "#FF8C33"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+  });
+    
